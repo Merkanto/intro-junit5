9e632cafb017a79b57fdc729624bb90c41b81f84
Merkanto <miroslav.i.zhelezchev@gmail.com>
2023-01-03 06:56:47 +0200

    Init commit.

---
+++ src/main/java/merkanto/introjunit5/IntroJunit5Application.java
@@ -0,0 +1,13 @@
+package merkanto.introjunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroJunit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroJunit5Application.class, args);
+    }
+
+}
